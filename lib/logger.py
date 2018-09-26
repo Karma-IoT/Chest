137866b17eb642e9d2de09c4be88cab580d31abb
tiannian <dtiannian@aliyun.com>
2018-09-26 23:16:27 +0800

    merge library and change this repo to be specs

--- lib/logger.py
+++
@@ -1,7 +0,0 @@
-import logging
-
-logging.basicConfig(level = logging.INFO,format='%(asctime)s %(message)s',datefmt='%Y-%m-%d %H:%M:%S')
-
-def info(msg, *args, **kwargs):
-    logging.info('-- [KBT] ' + msg, *args, **kwargs)
-
